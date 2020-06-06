7bd02ffa5c13c8a1fc684827830778c0f6ccf525
Will Thomson <will.tho@outlook.com>
2020-06-07 01:00:25 +1200

    Add problem 372

---
+++ 372.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+typedef unsigned int uint;
+
+uint digits(uint n) {
+    if (n == 0) return 1;
+    return (uint)floor(log10(n)) + 1;
+}
+
+int main() {
+    uint n;
+    cin >> n;
+    cout << digits(n) << endl;
+}
